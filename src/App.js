e7c34c6865698ff4b533b2cfc955e329036a6b3b
Syed Abdul Aziz <92917995+syedazeez256@users.noreply.github.com>
2022-07-16 15:24:53 +0500

    passing props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
 import ExpenseItems from "./components/ExpenseItems";
 
 function App() {
+  const expense = [
+    {
+      id: 1,
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    {
+      id: 2,
+      title: "Car Insurance",
+      amount: 294.97,
+      date: new Date(2021, 2, 13),
+    },
+    {
+      id: 3,
+      title: "New Desk",
+      amount: 100.12,
+      date: new Date(2021, 9, 10),
+    },
+  ];
   return (
     <div>
       <h2>Let's get started!</h2>
       <p>This is paragraph !</p>
-      <ExpenseItems></ExpenseItems>
+      <ExpenseItems
+        title={expense[0].title}
+        amount={expense[0].amount}
+        date={expense[0].date}
+      ></ExpenseItems>
+      <ExpenseItems
+        title={expense[1].title}
+        amount={expense[1].amount}
+        date={expense[1].date}
+      ></ExpenseItems>
+      <ExpenseItems
+        title={expense[2].title}
+        amount={expense[2].amount}
+        date={expense[2].date}
+      ></ExpenseItems>
     </div>
   );
 }
